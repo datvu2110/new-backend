91f03bef2481cf0b7fbc3ee71459b30f13c99375
Derek Vu <beetee@Dereks-MacBook-Pro.local>
2020-12-17 17:37:25 -0600

    add test

--- src/app.js
+++ src/app.js
@@ -27,6 +27,9 @@ app.use(cors())
 app.use(bodyParser.json())
 
 
+app.get('/', (req, res) => {
+	res.send("Hello World")
+})
 
 app.get('/users', (req, res) => {
 	db.select('email').from('users').then((users) => {
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,10 +1,22 @@
-
+const {expect} = require('chai')
+const supertest = require('supertest')
 const app = require('../src/app')
+const sinon = require('sinon')
+const knex = require('knex')
 
-describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
-    return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, world!')
-  })
+describe('App', ()=>{
+    it('GET /api responds with 200 containing "Hello World"', ()=> {
+        return supertest(app)
+           .get('/')
+           .expect(200, 'Hello World')
+    })
+})
+
+describe('App', ()=>{
+    it('Test users', ()=> {
+        sinon.stub(knex, "from").callsFake( (any) => {return {"email" : "email@email.com"}; })
+        return supertest(app)
+           .get('/users')
+           .expect(200, 'Hello World')
+    })
 })
