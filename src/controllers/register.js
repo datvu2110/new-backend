ebcf202b9573cab7c17b41acc0b63aa7d183e5c0
Derek Vu <beetee@Dereks-MacBook-Pro.local>
2021-03-05 17:49:30 -0600

    register in separate file

---
+++ src/controllers/register.js
@@ -0,0 +1,33 @@
+const handleRegister = (req,res,db,bcrypt) => {
+    const {email, name, password} = req.body
+    const hash = bcrypt.hashSync(password)
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                email:loginEmail[0],
+                name:name
+            }).then(user => {
+                res.json(user[0])
+                
+            }).catch(error => {
+                res.send('unable to register')
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => console.log(err))
+   
+}
+
+module.exports = {
+    handleRegister:handleRegister
+}
